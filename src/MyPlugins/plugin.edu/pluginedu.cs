cfffb41bdf19cb5785fd33b7c4148c01b4705b6e
umut.ulupinar <umutulupinarrr@gmail.com>
2023-10-20 17:31:43 +0300

    Step4: implemented payment methods

--- src/MyPlugins/plugin.edu/pluginedu.cs
+++ src/MyPlugins/plugin.edu/pluginedu.cs
@@ -1,8 +1,87 @@
-﻿using Nop.Services.Plugins;
+﻿using Microsoft.AspNetCore.Http;
+using Nop.Core.Domain.Orders;
+using Nop.Services.Payments;
+using Nop.Services.Plugins;
 
 namespace plugin.edu;
 
-public class pluginedu : BasePlugin
+public class pluginedu : BasePlugin , IPaymentMethod
 {
-    
+    public Task<ProcessPaymentResult> ProcessPaymentAsync(ProcessPaymentRequest processPaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task PostProcessPaymentAsync(PostProcessPaymentRequest postProcessPaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> HidePaymentMethodAsync(IList<ShoppingCartItem> cart)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<decimal> GetAdditionalHandlingFeeAsync(IList<ShoppingCartItem> cart)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CapturePaymentResult> CaptureAsync(CapturePaymentRequest capturePaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<RefundPaymentResult> RefundAsync(RefundPaymentRequest refundPaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<VoidPaymentResult> VoidAsync(VoidPaymentRequest voidPaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProcessPaymentResult> ProcessRecurringPaymentAsync(ProcessPaymentRequest processPaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<CancelRecurringPaymentResult> CancelRecurringPaymentAsync(CancelRecurringPaymentRequest cancelPaymentRequest)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CanRePostProcessPaymentAsync(Order order)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IList<string>> ValidatePaymentFormAsync(IFormCollection form)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ProcessPaymentRequest> GetPaymentInfoAsync(IFormCollection form)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Type GetPublicViewComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> GetPaymentMethodDescriptionAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SupportCapture { get; }
+    public bool SupportPartiallyRefund { get; }
+    public bool SupportRefund { get; }
+    public bool SupportVoid { get; }
+    public RecurringPaymentType RecurringPaymentType { get; }
+    public PaymentMethodType PaymentMethodType { get; }
+    public bool SkipPaymentInfo { get; }
 }
